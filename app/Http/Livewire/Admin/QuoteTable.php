4fcdcebb05fa7ba0b5fc3d58d97d37ad4587e74f
bernardo-campos <camposbg@gmail.com>
2026-05-17 21:25:38 -0300

    fix: sort by author name

--- app/Http/Livewire/Admin/QuoteTable.php
+++ app/Http/Livewire/Admin/QuoteTable.php
@@ -25,14 +25,7 @@ class QuoteTable extends DataTableComponent
                 ->sortable()
                 ->searchable(),
             Column::make(__('Author'), 'author.name')
-                ->sortable(
-                    function(Builder $query, $direction) {
-                        return $query
-                            ->leftJoin('authors','authors.id','=','quotes.author_id')
-                            ->select('quotes.*', 'authors.name')
-                            ->orderBy('authors.name', $direction);
-                    }
-                )
+                ->sortable()
                 ->searchable(),
             Column::make(__('Quote'), 'quote')
                 ->sortable()
